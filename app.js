60ea36f6cd40f9a108f4f58ed4a6d0fa04670715
Patrick Brown <pjbrown05@gmail.com>
2020-07-09 23:50:08 -0500

    create arrays based on user input

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
-// const Manager = require('./lib/Manager');
-// const Engineer = require('./lib/Engineer');
-// const Intern = require('./lib/Intern');
+const Manager = require('./lib/Manager');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
 const inquirer = require('inquirer');
-// const path = require('path');
-// const fs = require('fs');
+const path = require('path');
+const fs = require('fs');
 
-// const OUTPUT_DIR = path.resolve(__dirname, 'output');
-// const outputPath = path.join(OUTPUT_DIR, 'team.html');
+const OUTPUT_DIR = path.resolve(__dirname, 'output');
+const outputPath = path.join(OUTPUT_DIR, 'team.html');
 
-// const render = require('./lib/htmlRenderer');
+const render = require('./lib/htmlRenderer');
 
 
 // Write code to use inquirer to gather information about the development team members,
@@ -41,7 +41,7 @@ inquirer.prompt([{
             }
         ])
         .then((managerRole) => {
-            console.log(response, managerRole);
+            // console.log(response, managerRole);
             let newManager = {
                 ...response,
                 ...managerRole
@@ -49,7 +49,7 @@ inquirer.prompt([{
             console.log(newManager)
         }) 
     } 
-    if (response.role === 'Engineer') {
+    else if (response.role === 'Engineer') {
         inquirer.prompt([{
             type: 'input',
             name: 'github',
@@ -57,34 +57,35 @@ inquirer.prompt([{
         }
     ])
     .then((engineerRole) => {
-        console.log(response, engineerRole);
-        let newEngineer = {
-            ...response,
-            ...engineerRole
+        // console.log(response, engineerRole);
+            let newEngineer = {
+                ...response,
+                ...engineerRole
+            }
+            console.log(newEngineer)
+        }) 
+    }
+    else if (response.role === 'Intern') {
+        inquirer.prompt([{
+            type: 'input',
+            name: 'school',
+            message: 'What school your employee attend?',
         }
-        console.log(newEngineer)
-    }) 
-}
-    // else (response.role === 'Engineer') {
-    //         inquirer.prompt([{
-    //             type: 'input',
-    //             name: 'github',
-    //             message: 'What is your employee\'s GitHub username?',
-    //         }
-    //     ])
-    // }
-    // else (response.role === 'Intern') {
-    //         inquirer.prompt([{
-    //             type: 'input',
-    //             name: 'school',
-    //             message: 'What school did your employee attend?',
-    //         }
-    //     ])
-    // }
+    ])
+    .then((internRole) => {
+        // console.log(response, internRole);
+            let newIntern = {
+                ...response,
+                ...internRole
+            }
+            console.log(newIntern)
+        }) 
+    }
 })
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
 // generate and return a block of HTML including templated divs for each employee!
+
 
 // After you have your html, you're now ready to create an HTML file using the HTML
 // returned from the `render` function. Now write it to a file named `team.html` in the
