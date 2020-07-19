81407d6e39d24648a0300efa4bdcc86fa62f1daa
Patrick Brown <pjbrown05@gmail.com>
2020-07-19 13:24:48 -0500

    renders html

--- app.js
+++ app.js
@@ -10,35 +10,34 @@ const outputPath = path.join(OUTPUT_DIR, 'team.html');
 
 const render = require('./lib/htmlRenderer');
 
+const employees = [];
 
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
 function createManager() {
 inquirer.prompt([
   {
     type: 'input',
     name: 'name',
-    message: 'What is your manager\'s name?',
+    message: 'What is your manager\'s name?'
   }, 
   {
     type: 'input',
     name: 'id',
-    message: 'What is your manager\'s ID number?',
+    message: 'What is your manager\'s ID number?'
   }, 
   {
     type: 'input',
     name: 'email',
-    message: 'What is your manager\'s email address?',
+    message: 'What is your manager\'s email address?'
   },
   {
     type: 'input',
     name: 'officeNumber',
-    message: 'What is your manager\'s office number?',
+    message: 'What is your manager\'s office number?'
   }
 ])
 .then(response => {
     let manager = new Manager(response.name, response.id, response.email, response.officeNumber);
-    console.log(manager);
+    // console.log(manager);
     employees.push(manager);
     addEmployee();
     }   
@@ -55,12 +54,14 @@ inquirer.prompt([
 ])
 .then(response => {
     switch (response.role) {
-        case "Engineer":
+        case 'Engineer':
             addEngineer();
             break;
-         case "Intern":
-             addIntern();
-        default:
+        case 'Intern':
+            addIntern();
+            break;
+        case 'Done adding employees':
+            generateHtml();
             break;
      }
    })
@@ -85,13 +86,13 @@ function addEngineer() {
     }, 
     {
         type: 'input',
-        name: 'github',
+        name: 'gitHub',
         message: 'What is your engineer\'s GitHub username?'
     }
     ])
     .then(response => {
-        let engineer = new Engineer(response.name, response.id, response.email, response.github);
-        console.log(engineer);
+        let engineer = new Engineer(response.name, response.id, response.email, response.gitHub);
+        // console.log(engineer);
         employees.push(engineer);
         addEmployee();
     }
@@ -123,34 +124,16 @@ function addIntern() {
     ])
     .then(response => {
         let intern = new Intern(response.name, response.id, response.email, response.school);
-        console.log(intern);
+        // console.log(intern);
         employees.push(intern);
         addEmployee();
     }
 )}
 
+function generateHtml() {
+    fs.writeFile(outputPath, render(employees), (err) => {
+        if (err) throw err
+    })
+}
 
-
-
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
-
-
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
-
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
-
-createManager()
+createManager();
